84cd076ea5cd3010aa7831bd555a7fc693ea4652
tuotuoxp <tuotuoxp@gmail.com>
2016-11-06 23:15:38 +0800

    update submodule add merge script

---
+++ merge_upstream.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set dir=%~dp0
+cd /D %dir%
+
+cd %dir%\torch7
+git remote add upstream https://github.com/torch/torch7.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
+cd %dir%\nn
+git remote add upstream https://github.com/torch/nn.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
+cd %dir%\cutorch
+git remote add upstream https://github.com/torch/cutorch.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
+cd %dir%\cunn
+git remote add upstream https://github.com/torch/cunn.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
---
+++ merge_upstream.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+basedir=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
+
+
+cd $basedir/torch7
+git remote add upstream https://github.com/torch/torch7.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
+cd $basedir/nn
+git remote add upstream https://github.com/torch/nn.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
+cd $basedir/cutorch
+git remote add upstream https://github.com/torch/cutorch.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
+cd $basedir/cunn
+git remote add upstream https://github.com/torch/cunn.git
+git fetch upstream
+git checkout master
+git merge --no-edit upstream/master
+
