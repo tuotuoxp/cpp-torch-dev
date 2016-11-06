c1eb8c03a98f7585342eada0b916d182c1688404
tuotuoxp <tuotuoxp@gmail.com>
2016-11-06 23:37:20 +0800

    modify merge upstream

--- merge_upstream.bat
+++ merge_upstream.bat
@@ -7,23 +7,27 @@ cd %dir%\torch7
 git remote add upstream https://github.com/torch/torch7.git
 git fetch upstream
 git checkout master
+git pull --no-edit
 git merge --no-edit upstream/master
 
 cd %dir%\nn
 git remote add upstream https://github.com/torch/nn.git
 git fetch upstream
 git checkout master
+git pull --no-edit
 git merge --no-edit upstream/master
 
 cd %dir%\cutorch
 git remote add upstream https://github.com/torch/cutorch.git
 git fetch upstream
 git checkout master
+git pull --no-edit
 git merge --no-edit upstream/master
 
 cd %dir%\cunn
 git remote add upstream https://github.com/torch/cunn.git
 git fetch upstream
 git checkout master
+git pull --no-edit
 git merge --no-edit upstream/master
 
