ff9bffc7fec515e84999b75e8346cd914ca3106f
tuotuoxp <tuotuoxp@gmail.com>
2016-11-06 14:27:04 -0500

    chmod, update sh

--- build_deps.sh
+++ build_deps.sh
@@ -13,15 +13,13 @@ fi
 mkdir -p $basedir/build/TH
 cd $basedir/build/TH
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$dest $basedir/torch7/lib/TH
-make
-make install
+make && make install
 
 
 mkdir -p $basedir/build/THNN
 cd $basedir/build/THNN
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$dest -DCMAKE_INSTALL_PREFIX=$dest $basedir/nn/lib/THNN
-make
-make install
+make && make install
 
 
 cd $basedir
--- build_make.sh
+++ build_make.sh
@@ -14,8 +14,7 @@ mkdir -p $basedir/build/cpp-torch
 cd $basedir/build/cpp-torch
 enable_cuda=$(which cudafe > /dev/null 2>&1; echo $(expr $? == 0))
 cmake -DCMAKE_BUILD_TYPE=Release -DWITH_CUDA=$enable_cuda -DCMAKE_PREFIX_PATH=$dest -DCMAKE_INSTALL_PREFIX=$dest $basedir/cpp-torch
-make
-make install
+make && make install
 
 
 cd $basedir
