d769fedd5d0523727ce2e24c4ab1f41a42d2730f
tuotuoxp <tuotuoxp@gmail.com>
2016-11-15 23:22:36 +0800

    remove build test on macos

--- build_xcode.sh
+++ build_xcode.sh
@@ -6,7 +6,7 @@ dest=$basedir/install
 
 mkdir -p $basedir/build/cpp-torch
 cd $basedir/build/cpp-torch
-cmake -G "Xcode" -DBUILD_TESTS=ON -DCMAKE_PREFIX_PATH=$dest -DCMAKE_INSTALL_PREFIX=$dest $basedir/cpp-torch
+cmake -G "Xcode" -DCMAKE_PREFIX_PATH=$dest -DCMAKE_INSTALL_PREFIX=$dest $basedir/cpp-torch
 
 
 cd $basedir
