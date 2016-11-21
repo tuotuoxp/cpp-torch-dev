63a38060d87862d1e8122c67794151633948de29
tuotuoxp <tuotuoxp@gmail.com>
2016-11-21 21:04:36 +0800

    add fns's build to build_xcode.sh

--- build_xcode.sh
+++ build_xcode.sh
@@ -4,10 +4,14 @@ basedir=$(cd "$(dirname "${BASH_SOURCE}")" && pwd)
 dest=$basedir/install
 
 
-mkdir -p $basedir/build/cpp-torch
-cd $basedir/build/cpp-torch
+mkdir -p $basedir/build/cpp-torch-xcode
+cd $basedir/build/cpp-torch-xcode
 cmake -G "Xcode" -DCMAKE_PREFIX_PATH=$dest -DCMAKE_INSTALL_PREFIX=$dest $basedir/cpp-torch
 
+
+mkdir -p $basedir/build/cpp-torch-fns
+cd $basedir/build/cpp-torch-fns
+cmake -G "Xcode" -DCMAKE_PREFIX_PATH=$dest $basedir/cpp-torch/example/fns
 
 cd $basedir
 
